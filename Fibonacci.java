d7d2927dcb6c80d00c403070fca94b7cf95568f8
Anshuman bhattacharya <77188112+Anshuman0301@users.noreply.github.com>
2021-06-09 00:10:30 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,30 @@
+package JavaCodes;
+
+import java.util.Scanner;
+
+public class Fibonacci
+{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+
+        int n= sc.nextInt();
+
+
+        int a=0;
+        int b=1;
+        int c;
+        System.out.print("0 1");
+
+        for(int i=2; i<n; i++)
+        {
+            c= a+b;
+            System.out.print(" "+c);
+            a=b;
+            b=c;
+
+        }
+
+
+    }
+
+}
